a1f323cfa8ddcf47f116846239827ae4dcc54226
Lizka <kuzo-liza@yandex.ru>
2016-03-14 01:13:10 +0400

    console app

---
+++ Sources/Console/console.h
@@ -0,0 +1,19 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+class console
+{
+public:
+    console();
+
+printContacts();
+printContactsInFile();
+
+};
+
+#endif // CONSOLE_H
