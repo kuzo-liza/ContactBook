696e3e451c589d6d875fcf97a5d65bab37792b58
Lizka <kuzo-liza@yandex.ru>
2016-04-25 01:30:57 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,17 +29,17 @@ build_debug_version() {
 	#qmake "QMAKE_CXXFLAGS+=-fprofile-arcs -ftest-coverage -fPIC -O0 -g --coverage" "LIBS+=-lgcov"
 	if [ -e "Makefile" ]; then
 		make --version
-		#make
+		make
 		#Test/tst_testcore -xml -o test_results || true
-		#cppcheck --version
-		#cppcheck --enable=all -v  --xml  * 2> ../../Reports/cppcheck_result
+		cppcheck --version
+		cppcheck --enable=all -v  --xml  * 2> ../../Reports/cppcheck_result
 		#gcovr --version
 		#gcovr -r . --xml --exclude='tst*' -o  ../../Reports/gcovr_result
 		
 		#valgrind --version
 		#valgrind --leak-check=full --xml=yes --xml-file=/opt/tomcat/.jenkins/jobs/ContactBook/workspace/tst_testcore.%p.result /opt/tomcat/.jenkins/jobs/ContactBook/workspace/Sources/ContactBook/Test/tst_testcore || true
 
-		#cd ../..
+		cd ../..
 	else
 		echo "Makefile does not exist"
 		echo "Build debug version failure!"
