a8bf702cc751c7fd519425e81a5c0ec91a02a952
Lizka <kuzo-liza@yandex.ru>
2016-05-27 11:57:09 +0400

    first layout, simple program

--- Sources/Contact-Book/GuiApp/main.cpp
+++ Sources/Contact-Book/GuiApp/main.cpp
@@ -4,6 +4,7 @@
 #include <QPushButton>
 #include <QSlider>
 #include <QSpinBox>
+#include <QHBoxLayout>
 
 int main(int argc, char *argv[])
 {
@@ -16,12 +17,24 @@ int main(int argc, char *argv[])
 
     QObject::connect(button, SIGNAL(clicked(bool)), &app, SLOT(quit()));
 
-//    QSpinBox *counter = new QSpinBox;
-//    QSlider *slider = new QSlider (Qt::Horizontal);
+    QSpinBox *counter = new QSpinBox;
+    QSlider *slider = new QSlider (Qt::Horizontal);
 
-//    counter->show();
-//    slider->show();
+    counter->setMaximum(100);
+    slider->setMaximum(100);
 
+    QObject::connect(counter, SIGNAL(valueChanged(int)), slider, SLOT(setValue(int)));
+    QObject::connect(slider, SIGNAL(valueChanged(int)), counter, SLOT(setValue(int)));
+
+    QWidget *window = new QWidget;
+    QHBoxLayout *layout = new QHBoxLayout;
+
+    layout->addWidget(counter);
+    layout->addWidget(slider);
+    layout->addWidget(button);
+
+    window->setLayout(layout);
+    window->show();
 
 //    QLabel label("Contact Book");
 //    label.show();
