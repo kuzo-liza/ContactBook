ad2bded3a8ff93db00319ae539f4ee17646c789b
Lizka <kuzo-liza@yandex.ru>
2016-06-03 03:47:02 +0400

    doxygen comments

--- Sources/Contact-Book/Engine/contact.h
+++ Sources/Contact-Book/Engine/contact.h
@@ -9,18 +9,47 @@
 #include <stdlib.h>
 
 using namespace std;
-
+/**
+ * @brief Класс Контакт, включающий в себя имя, фамилию и телефон контакта
+ */
 class Contact
 {
 
 public:
-
+    /**
+     * @brief Присвоение имени контакта
+     * @param Имя
+     */
     void setName(string name);
+
+    /**
+     * @brief Присвоение фамилии контакта
+     * @param Фамилия
+     */
     void setSurname(string surname);
+
+    /**
+     * @brief Присвоение телефона контакта
+     * @param Телефон
+     */
     void setNumber(string number);
 
+    /**
+     * @brief Получение имени контакта
+     * @return Имя
+     */
     string getName();
+
+    /**
+     * @brief Получение фамилии контакта
+     * @return Фамилия
+     */
     string getSurname();
+
+    /**
+     * @brief Получение телефона контакта
+     * @return Телефон
+     */
     string getNumber();
 
     string name;
@@ -30,7 +59,6 @@ public:
     int group;
 
     Contact();
-
     Contact(const Contact &value);
     //const &operator =(const Contact &value);
 };
--- Sources/Contact-Book/Engine/contact_book.h
+++ Sources/Contact-Book/Engine/contact_book.h
@@ -3,25 +3,83 @@
 
 #include "contact.h"
 
+/**
+ * @brief Класс Книга контактов
+ */
 class ContactBook
 {
 
 public:
 
     int sizeOfBook;
+
     ContactBook();
+
+    /**
+     * @brief Вектор из контактов
+     */
     vector <Contact> contactBook;
 
+    /**
+     * @brief Проверка количества контактов
+     * @return Количество контактов в книге
+     */
     int checkSizeOfFile();
+
+    /**
+     * @brief Удаление контакта
+     * @param Номер удаляемого контакта
+     */
     void deleteContact(int num);
+
+    /**
+     * @brief Поиск контакта по имени
+     * @param Имя
+     * @return Найденный контакт
+     */
     Contact searchByName(string name);
+
+    /**
+     * @brief Поиск контакта по фамилии
+     * @param Фамилия
+     * @return Найденный контакт
+     */
     Contact searchBySurname(string surname);
+
+    /**
+     * @brief Создание контакта
+     * @param Имя
+     * @param Фамилия
+     * @param Телефон
+     */
     void createContact(string name, string surname, string number);
+
+    /**
+     * @brief Заполнение книги
+     */
     void fillInTheBook();
+
+    /**
+     * @brief Сортировка контактов
+     */
     void sortContacts();
+
     bool compareByName(const Contact& contact1, const Contact& contact2);
+
+    /**
+     * @brief Сортировка по имени
+     */
     void sortByName();
+
+    /**
+     * @brief Проверка количества контактов в книге
+     * @return Размер книги
+     */
     int checkNumberOfContacts();
+
+    /**
+     * @brief Сохранение книги
+     */
     void saveBook();
 };
 
